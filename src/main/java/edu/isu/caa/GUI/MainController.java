4c85334cc88a65a79d5b5f764745ed91565d0586
Andres <sewellandres@gmail.com>
2019-12-03 15:30:59 -0700

    add

--- src/main/java/edu/isu/caa/GUI/MainController.java
+++ src/main/java/edu/isu/caa/GUI/MainController.java
@@ -6,6 +6,9 @@ import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 
 public class MainController implements Initializable {
@@ -13,8 +16,22 @@ public class MainController implements Initializable {
     @FXML
     ChoiceBox choiceBox;
 
+    @FXML
+    TableView GradeScaleTableView;
+
+    @FXML
+    TableColumn GradeScaleLetterColumn;
+
+    @FXML
+    TableColumn GradeScaleGradeColumn;
+
+    @FXML
+    TableColumn GradeScaleGPAColumn;
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        GradeScaleTableView.getItems().add("Hello is this working");
         choiceBox.getItems().clear();
         choiceBox.getItems().addAll("GPA Calculator", "How many credits at ... do I need to raise my GPA?", "What GPA would I need to raise my GPA to ...?");
         choiceBox.getSelectionModel().select(0);
@@ -24,5 +41,9 @@ public class MainController implements Initializable {
     public void closeProgram(ActionEvent event) {
         event.consume();
         System.exit(0);
+    }
+
+    public class Scale {
+        String GPA;
     }
 }
