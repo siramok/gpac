85a1f6bdb1964868b5a47d409935431048ad1cb3
Andres <sewellandres@gmail.com>
2019-12-04 14:29:44 -0700

    Made the course list tableView functional with options

--- src/main/java/edu/isu/caa/GUI/MainController.java
+++ src/main/java/edu/isu/caa/GUI/MainController.java
@@ -3,51 +3,46 @@ package edu.isu.caa.GUI;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.Property;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.Callback;
 
 
 public class MainController implements Initializable {
-    public class Scale {
+    public class TableEntry {
         String letter;
         String range;
         String number;
+        String course;
 
-        public Scale(String letter, String range, String number) {
+        public TableEntry(String course) {
+            this.course = course;
+        }
+
+        public TableEntry(String letter, String range, String number) {
             this.letter = letter;
             this.range = range;
             this.number = number;
         }
 
-        public String getLetter() {
-            return letter;
-        }
-
-        public void setLetter(String letter) {
-            this.letter = letter;
-        }
-
-        public String getRange() {
-            return range;
-        }
-
-        public void setRange(String range) {
-            this.range = range;
-        }
-
-        public String getNumber() {
-            return number;
-        }
-
-        public void setNumber(String number) {
-            this.number = number;
-        }
+        public String getLetter() { return letter; }
+        public void setLetter(String letter) { this.letter = letter; }
+        public String getRange() { return range; }
+        public void setRange(String range) { this.range = range; }
+        public String getNumber() { return number; }
+        public void setNumber(String number) { this.number = number; }
+        public String getCourse() { return course; }
+        public void setCourse(String course) { this.course = course; }
     }
 
     @FXML
@@ -62,27 +57,65 @@ public class MainController implements Initializable {
     @FXML
     TableColumn GradeScaleNumberColumn;
 
+    @FXML
+    TableView CourseTableView;
+
+    @FXML
+    TableColumn CourseColumn;
+
+    @FXML
+    TableColumn CreditHoursColumn;
+
+    @FXML
+    TableColumn ExpectedGradeColumn;
+
+    @FXML
+    TableColumn RetakeColumn;
+
+    @FXML
+    TableColumn OriginalGradeColumn;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        ObservableList<Scale> observableList = FXCollections.observableArrayList(
-                new Scale("A ", "93-100", "4.0"),
-                new Scale("A-", "90-92.99", "3.7"),
-                new Scale("B+", "87-89.99", "3.3"),
-                new Scale("B ", "83-86.99", "3.0"),
-                new Scale("B-", "80-82.99", "2.7"),
-                new Scale("C+", "77-79.99", "2.3"),
-                new Scale("C ", "73-76.99", "2.0"),
-                new Scale("C-", "70-72.99", "1.7"),
-                new Scale("D+", "67-69.99", "1.3"),
-                new Scale("D ", "65-66.99", "1.0"),
-                new Scale("F ", "0-64.99", "0.0")
+        ObservableList<TableEntry> scaleList = FXCollections.observableArrayList(
+                new TableEntry("A ", "93-100", "4.0"),
+                new TableEntry("A-", "90-92.99", "3.7"),
+                new TableEntry("B+", "87-89.99", "3.3"),
+                new TableEntry("B ", "83-86.99", "3.0"),
+                new TableEntry("B-", "80-82.99", "2.7"),
+                new TableEntry("C+", "77-79.99", "2.3"),
+                new TableEntry("C ", "73-76.99", "2.0"),
+                new TableEntry("C-", "70-72.99", "1.7"),
+                new TableEntry("D+", "67-69.99", "1.3"),
+                new TableEntry("D ", "65-66.99", "1.0"),
+                new TableEntry("F ", "0-64.99", "0.0")
         );
+
+        ObservableList<TableEntry> courseList = FXCollections.observableArrayList(
+                new TableEntry(""),
+                new TableEntry(""),
+                new TableEntry(""),
+                new TableEntry(""),
+                new TableEntry(""),
+                new TableEntry(""),
+                new TableEntry(""),
+                new TableEntry(""),
+                new TableEntry(""),
+                new TableEntry(""),
+                new TableEntry("")
+                );
 
         GradeScaleLetterColumn.setCellValueFactory(new PropertyValueFactory<>("Letter"));
         GradeScaleRangeColumn.setCellValueFactory(new PropertyValueFactory<>("Range"));
         GradeScaleNumberColumn.setCellValueFactory(new PropertyValueFactory<>("Number"));
-        GradeScaleTableView.setItems(observableList);
+        GradeScaleTableView.setItems(scaleList);
+
+        CourseColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        CreditHoursColumn.setCellFactory(ComboBoxTableCell.forTableColumn("", "0", "1", "2", "3", "4", "5"));
+        ExpectedGradeColumn.setCellFactory(ComboBoxTableCell.forTableColumn("", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "F"));
+        RetakeColumn.setCellFactory(ComboBoxTableCell.forTableColumn("", "Yes", "No"));
+        OriginalGradeColumn.setCellFactory(ComboBoxTableCell.forTableColumn("", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "F"));
+        CourseTableView.setItems(courseList);
 
     }
 
@@ -91,6 +124,4 @@ public class MainController implements Initializable {
         event.consume();
         System.exit(0);
     }
-
-
 }
